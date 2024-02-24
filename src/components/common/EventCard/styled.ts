838798ab80872cd935d42e7e172268525bb94e14
Milgam06 <milgamfruit@gmail.com>
2024-02-25 04:18:10 +0900

    feat(component): separate EventSectionContent into EventCard

---
+++ src/components/common/EventCard/styled.ts
@@ -0,0 +1,20 @@
+import { StaticImageData } from "next/image";
+
+import styled from "@emotion/styled";
+
+export const EventCard = styled.div<{ src: StaticImageData }>`
+  width: 36rem;
+  height: 22rem;
+  display: flex;
+  flex-direction: column;
+  border-radius: 2rem;
+  padding: 1.4rem 2rem;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
+    url(${({ src }) => src.src});
+  background-size: cover;
+  background-position: center;
+  cursor: default;
+  @media screen and (max-width: 767px) {
+    width: 100%;
+  }
+`;
