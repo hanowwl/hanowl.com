3ebf89786808c1be46a53508714f008fc0566c0c
imratatouille <130735410+imratatouille@users.noreply.github.com>
2024-02-26 05:21:38 +0900

    fix(style): Intro Dept Box style fix

--- src/components/common/IntroDeptBox/index.tsx
+++ src/components/common/IntroDeptBox/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import * as I from "src/assets";
 
 import { IntroMenuItem } from "src/components/main/IntroDeptSection";
 
@@ -12,24 +13,21 @@ export type IntroDeptBoxProps = IntroDeptBoxCustomProps & React.HTMLAttributes<H
 export const IntroDeptBox: React.FC<IntroDeptBoxProps> = ({
   text,
   icon,
-  intro,
+  introFirst,
   introSecond,
-  arrow,
 }) => {
   return (
     <S.IntroDeptBox>
       <S.IntroDeptsecondbox>
         <S.IntroTeamIcon src={icon} alt={text} quality={100} />
-        <Text size={1.3}>{text}</Text>
-        <Text size={2.2} weight={700}>
-          {intro}
+        <Text size={1.2}>{text}</Text>
+        <Text size={1.8} weight={600}>
+          {introFirst}
           <br />
           {introSecond}
+          <S.ArrowIcon src={I.arrow} alt={text} quality={100} />
         </Text>
       </S.IntroDeptsecondbox>
-      <S.IconBox>
-        <S.ArrowIcon src={arrow} alt={text} quality={100} />
-      </S.IconBox>
     </S.IntroDeptBox>
   );
 };
--- src/components/common/IntroDeptBox/styled.ts
+++ src/components/common/IntroDeptBox/styled.ts
@@ -6,47 +6,31 @@ import styled from "@emotion/styled";
 
 export const IntroDeptBox = styled.div`
   background-color: ${colors.card};
-  padding: 4rem 15px;
+  padding: 1rem 15px;
   border-radius: 1.2rem;
-  transition: background-color 180ms;
   @media screen and (max-width: 767px) {
-    padding: 1.2rem 5px;
+    padding: 0.5rem 5px;
   }
 `;
 
 export const IntroDeptsecondbox = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: -30px;
-  transition: background-color 180ms;
+  padding: 1rem;
   @media screen and (max-width: 767px) {
-    padding: 3.2rem 5px;
+    padding: 0.5rem 5px;
   }
-`;
-
-export const IconBox = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
 `;
 
 export const ArrowIcon = styled(Image)`
-  margin-bottom: -20px;
-  display: flex;
+  float: right;
   width: 1.3rem;
   height: 1.6rem;
-  @media screen and (max-width: 767px) {
-    margin-bottom: 0px;
-    width: 1rem;
-    height: 1.3rem;
-  }
 `;
 
 export const IntroTeamIcon = styled(Image)`
   width: 4.6rem;
   height: 4.6rem;
   @media screen and (max-width: 767px) {
-    width: 2.4rem;
-    height: 2.4rem;
+    width: 1.4rem;
+    height: 1.4rem;
   }
 `;
