394aac88ecac078c11fa1115f7c3a31789d2eac1
imratatouille <130735410+imratatouille@users.noreply.github.com>
2024-03-05 05:01:07 +0900

    fix: github build failed line fix

--- src/components/common/TeamBox/index.tsx
+++ src/components/common/TeamBox/index.tsx
@@ -1,8 +1,8 @@
-import * as I from "src/assets";
-
 import { useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import { StaticImageData } from "next/image";
+
+import * as I from "src/assets";
 
 import { TEAM_ID_TO_TEXT, TEAM_TEXT_TO_ID } from "src/constant";
 import { useCheckAppStore } from "src/store";
